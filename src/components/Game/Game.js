f5bf9258bcc79875c3948ed684211f8069efb23e
Miles Lacek <mileslacek@gmail.com>
2019-10-07 09:04:37 -0500

    Imported React to all components and Imported Connect to connect them all.

---
+++ src/components/Game/Game.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+class Game extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+// mapping the state to props
+const mapStateToProps = reduxStore => {
+  return {
+    reduxStore
+  };
+};
+
+//exports the component
+export default connect(mapStateToProps)(Game);
