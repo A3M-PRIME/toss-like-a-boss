21dcc38e1683c0319159c82eaad537597e606241
Andy DuBois <andydubois89@gmail.com>
2019-10-07 11:11:58 -0500

    set up leaderboard table

--- src/components/LeaderBoardCompany/LeaderBoardCompany.js
+++ src/components/LeaderBoardCompany/LeaderBoardCompany.js
@@ -1,11 +1,54 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+
+//Material UI Components
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+//Material UI styles
+const styles = theme => ({
+  close: {
+    padding: theme.spacing.unit / 2
+  }
+});
 
 class Leaderboard extends Component {
-    render() {
-        return (
-            "Leadeboard"
-        )
-    }
+  render() {
+          const { classes } = this.props;
+    return (
+      <>
+        <Grid container spacing={24}>
+          <Grid item xs>
+            <Paper className={classes.paper}></Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Button>PLAY AGAIN</Button>
+            <Paper>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Score</TableCell>
+                    <TableCell>Time</TableCell>
+                    <TableCell>Department</TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </Paper>
+          </Grid>
+          <Grid item xs>
+            <Paper className={classes.paper}></Paper>
+          </Grid>
+        </Grid>
+      </>
+    );
+  }
 }
 
-export default Leaderboard;
+export default withStyles(styles)(Leaderboard);
