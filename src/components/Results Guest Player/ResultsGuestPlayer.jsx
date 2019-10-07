339750f80c65feaa6bb3284e52392c34de694292
Michael O'Donnell <gorillaejones@gmail.com>
2019-10-07 10:01:01 -0500

    guest results in work

--- src/components/Results Guest Player/ResultsGuestPlayer.jsx
+++ src/components/Results Guest Player/ResultsGuestPlayer.jsx
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import {withStyles} from '@material-ui'
+import {connect} from 'react-redux';
+import { withStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+
+}
 
 class ResultsGuestPlayer extends Component {
     render() {
         return (
             <div>
-                
+                <div>
+                    <Typography component="h1">
+                        GAME RESULTS
+                    </Typography>
+                </div>
+                <div>
+                    <Typography component="h1">
+                        SCORE: 12/15 80%
+                    </Typography>
+                </div>
+                <div>
+                    <Typography component="body1">
+                        These items were sorted incorrectly:
+                    </Typography>
+                </div>
+                <div>
+                    <Button variant="contained">CLICK HERE TO PLAY AGAIN</Button>
+                </div>
             </div>
         );
     }
 }
 
-export default ResultsGuestPlayer;
+const mapStateToProps = (reduxStore) => {
+    return {
+        reduxStore
+    }
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(ResultsGuestPlayer));
