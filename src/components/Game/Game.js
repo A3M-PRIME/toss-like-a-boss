44e119a3667d1582d583c4cf86adcef488aee1c0
Miles Lacek <mileslacek@gmail.com>
2019-10-08 10:08:16 -0500

    Added styling to reflect the scope.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -12,6 +12,20 @@ const styles = {
     color: "white",
     border: "2px solid black",
     fontSize: "calc(10px + 2vmin)",
+    padding: "5px",
+    margin: "50px",
+    "&:hover": {
+      // change to both KEY and the
+      // textDecoration: "underline",
+      backgroundColor: "yellow",
+      color: "black"
+    }
+  },
+  h1: {
+    border: "2px solid black",
+    width: "100%",
+    margin: "20px",
+    padding: "40px"
   }
 };
 
@@ -23,6 +37,20 @@ class Game extends Component {
     });
   }
 
+  // route the user back to the home page
+  backToHome = () => {
+    this.props.history.push('/'); 
+  };
+
+  // route the user back to the how to play page
+  howToPlay = () => {
+    this.props.history.push('/howtoplay')
+  }
+
+  toReady = () => {
+    this.props.history.push('/gamelaunch')
+  }
+
   render() {
     return (
       <div>
@@ -32,8 +60,17 @@ class Game extends Component {
           spacing={12}
           alignItems={"center"}
         >
-          <h1>WASTE-WISE-R</h1> <h3>items remaining : 15</h3>{" "}
-          <h3>Elapsed Time showing : 0:00</h3>
+          <div className={this.props.classes.h1}>
+            <Grid
+              container
+              justify={"space-evenly"}
+              spacing={10}
+              alignItems={"center"}
+            >
+              <h1>WASTE-WISE-R</h1>
+              <h3>items remaining : 15</h3> <h3>Elapsed Time showing : 0:00</h3>
+            </Grid>
+          </div>
         </Grid>
         <body>
           <Grid
@@ -42,14 +79,18 @@ class Game extends Component {
             spacing={6}
             alignItems={"center"}
           >
-            <Button className={this.props.classes.Button} onClick>
-              {" "}
-              How To Play{" "}
+            <Button
+              className={this.props.classes.Button}
+              onClick={this.howToPlay}
+            >
+              How To Play
             </Button>
             <h2>Score : 0</h2>
-            <Button className={this.props.classes.Button} onClick>
-              {" "}
-              Back To Home{" "}
+            <Button
+              className={this.props.classes.Button}
+              onClick={this.backToHome}
+            >
+              Back To Home
             </Button>
           </Grid>
           <br></br>
@@ -60,13 +101,23 @@ class Game extends Component {
             alignItems={"center"}
           >
             <Button className={this.props.classes.Button} onClick>
-              {" "}
-              READY?!{" "}
+              READY?!
             </Button>
           </Grid>
         </body>
-        {JSON.stringify(this.props.reduxStore)}
-        <footer>- Receptacles - Trash/Garbage - Recycle - Compost</footer>
+        {/* {JSON.stringify(this.props.reduxStore)} */}
+        <footer>
+          <Grid
+            container
+            justify={"space-evenly"}
+            spacing={48}
+            alignItems={"center"}
+          >
+            <div className={this.props.classes.bin}>Trash/Garbage</div>
+            <div className={this.props.classes.bin}>Recycle</div>
+            <div className={this.props.classes.bin}>Compost</div>
+          </Grid>
+        </footer>
       </div>
     );
   }
