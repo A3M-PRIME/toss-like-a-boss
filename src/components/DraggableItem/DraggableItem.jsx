3062be62c66fba7e749e536fdf7de8d7e128e17b
Michael O'Donnell <gorillaejones@gmail.com>
2019-10-11 09:14:53 -0500

    removed unused code

--- src/components/DraggableItem/DraggableItem.jsx
+++ src/components/DraggableItem/DraggableItem.jsx
@@ -1,68 +1,3 @@
-// import React, { Component } from 'react';
-// import { DragSource } from 'react-dnd';
-// import {connect} from 'react-redux';
-
-// const Types = {
-//     ITEM: 'trash'
-// }
-
-// const style = {
-//     border: '1px dashed gray',
-//     backgroundColor: 'white',
-//     padding: '0.5rem 1rem',
-//     marginRight: '1.5rem',
-//     marginBottom: '1.5rem',
-//     cursor: 'move',
-//     float: 'left',
-// }
-
-// const itemSource = {
-//     beginDrag(props) {
-//         console.log('this is dragging');
-//         console.log(this.props)
-//         const item = {id: props.id}
-//         return item
-//     },
-
-//     endDrag(props, monitor, component) {
-//         const dropResult = monitor.getDropResult()
-//         if (!monitor.didDrop()) {
-//             console.log('drop')
-//             console.log('drop result is', dropResult)
-//             // alert('an alert')
-//             return
-//         }
-//     }
-// }
-
-// function collect(connect, monitor) {
-//     return {
-//         connectDragSource: connect.dragSource(),
-//         connectDragPreview: connect.dragPreview(),
-//         isDragging: monitor.isDragging(),
-//     }
-// }
-
-// class DraggableItem extends Component {
-//     render() {
-//         const { name, isDropped, isDragging, connectDragSource } = this.props;
-//         const opacity = isDragging ? 0 : 1;
-
-//         return connectDragSource(
-//             <div className='item' style={{...style, opacity}}>{isDropped ? <s>{name}</s> : name}
-//                 {/* <span>{this.props.gameItems[0].name}</span> */}
-//             </div>
-//         );
-//     }
-// }
-
-// const mapStateToProps = (reduxStore) => {
-//     return {
-//         gameItems: reduxStore.gameItemsReducer
-//     }
-// }
-// export default DragSource(Types.ITEM, itemSource, collect)(connect(mapStateToProps)(DraggableItem));
-
 import React, { useState } from "react";
 import ItemTypes from "../ItemTypes/ItemTypes";
 import { DragSource } from "react-dnd";
@@ -83,21 +18,6 @@ let currentGameValue = 0;
 
 const DraggableItem = ({ name, isDragging, connectDragSource, label }) => {
   const opacity = isDragging ? 0 : 1;
-
-  // function firstTryCorrect() {
-  //     dispatch({
-  //         type: "FIRST_TRY_CORRECT",
-  //         payload: itemId
-  //     });
-  // }
-
-  // function firstTryIncorrect() {
-  //     this.props.dispatch({
-  //         type: "FIRST_TRY_INCORRECT",
-  //         // payload: this.props.id
-  //     });
-  //     console.log(this.props.id)
-  // }
 
   return (
     <div ref={connectDragSource} style={{ ...style, opacity }}>
