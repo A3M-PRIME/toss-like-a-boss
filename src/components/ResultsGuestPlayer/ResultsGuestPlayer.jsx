304a19e29c9bc1f7b79f452ba9a74df81a172ff2
Andy DuBois <andydubois89@gmail.com>
2019-10-11 11:42:17 -0500

    game logic changes

--- src/components/ResultsGuestPlayer/ResultsGuestPlayer.jsx
+++ src/components/ResultsGuestPlayer/ResultsGuestPlayer.jsx
@@ -1,54 +1,62 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import { withStyles } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-
-import ResultsItemCard from '../ResultsItemCard/ResultsItemCard';
+import ResultsItemCard from "../ResultsItemCard/ResultsItemCard";
 
 const styles = {
-    media:{
-        width: '100px',
-        height: '100px'
-    }
-}
+  media: {
+    width: "100px",
+    height: "100px"
+  }
+};
 
 class ResultsGuestPlayer extends Component {
-    render() {
-        const scorePercentage = parseInt((this.props.gameScore / 15) * 100);
-        console.log(scorePercentage);
-        return (
-            <div>
-                <div>
-                    <Typography variant="h3">
-                        GAME RESULTS
-                    </Typography>
-                </div>
-                <div>
-                    <Typography variant="h5">
-                        SCORE: {this.props.gameScore}/15 {scorePercentage}%
-                    </Typography>
-                </div>
-                <div>
-                    <ResultsItemCard />
-                </div>
-                <div>
-                    <Button 
-                    onClick={() => this.props.history.push('/gamelaunch')} variant="contained">CLICK HERE TO PLAY AGAIN</Button>
-                </div>
-                <div>
-                    <Button variant="contained">CHECK OUT THE LEADERBOARD</Button>
-                </div>
-            </div>
-        );
-    }
+
+
+  playAgain = action => {
+    this.props.dispatch({
+      type: "RESET_CURRENT_GAME_VALUE"
+    });
+    this.props.history.push("/gamelaunch");
+  };
+
+
+  render() {
+    const scorePercentage = parseInt((this.props.gameScore / 15) * 100);
+    console.log(scorePercentage);
+    return (
+      <div>
+        <div>
+          <Typography variant='h3'>GAME RESULTS</Typography>
+        </div>
+        <div>
+          <Typography variant='h5'>
+            SCORE: {this.props.gameScore}/15 {scorePercentage}%
+          </Typography>
+        </div>
+        <div>
+          <ResultsItemCard />
+        </div>
+        <div>
+          <Button onClick={this.playAgain} variant='contained'>
+            CLICK HERE TO PLAY AGAIN
+          </Button>
+        </div>
+        <div>
+          <Button variant='contained'>CHECK OUT THE LEADERBOARD</Button>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (reduxStore) => {
-    return {
-        gameScore: reduxStore.gameScoreReducer,
-    }
-}
+const mapStateToProps = reduxStore => {
+  return {
+    gameScore: reduxStore.gameScoreReducer
+  };
+};
 
 export default connect(mapStateToProps)(withStyles(styles)(ResultsGuestPlayer));
--- src/redux/reducers/currentGameValueReducer.js
+++ src/redux/reducers/currentGameValueReducer.js
@@ -2,6 +2,8 @@ const currentGameValueReducer = (state = 0, action) => {
     switch (action.type) {
         case 'INCREMENT_CURRENT_GAME_VALUE':
             return state + 1;
+        case 'RESET_CURRENT_GAME_VALUE':
+            return 0;
         default:
             return state;
     }
