778379b3b666ecbca32d59ed72621ae2c5f4db55
Miles Lacek <mileslacek@gmail.com>
2019-10-14 13:49:09 -0500

    Added background to results page.

--- src/components/ResultsGuestPlayer/ResultsGuestPlayer.jsx
+++ src/components/ResultsGuestPlayer/ResultsGuestPlayer.jsx
@@ -10,6 +10,13 @@ const styles = {
     media: {
         width: "100px",
         height: "100px"
+    },
+    background: {
+        backgroundImage: "url(/images/River.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: 900,
+        padding: 24
     }
 };
 
@@ -38,35 +45,47 @@ class ResultsGuestPlayer extends Component {
         const scorePercentage = parseInt((this.props.gameScore / 15) * 100);
         console.log(scorePercentage);
         return (
-            <div>
-                <div>
-                    <Typography variant='h3'>GAME RESULTS</Typography>
-                </div>
-                <div>
-                    <Typography variant='h5'>
-                        SCORE: {this.props.gameWrongAnswers.length === undefined ? 15 : 15-this.props.gameWrongAnswers.length}/15 {parseInt(((15-this.props.gameWrongAnswers.length)/15)*100)}%
-          </Typography>
-                </div>
-                <div>
-                    <Typography variant='h5'>
-                        TIME: {this.props.gameTime}
-          </Typography>
-                </div>
-                <div>
-                    {this.props.history.location.search && <Button>CONTEST LEADERBOARD</Button>}
-                </div>
-                <div>
-                    <ResultsItemCard />
-                </div>
-                <div>
-                    <Button onClick={this.playAgain} variant='contained'>
-                        CLICK HERE TO PLAY AGAIN
-          </Button>
-                </div>
-                <div>
-                    <Button variant='contained'>CHECK OUT THE LEADERBOARD</Button>
-                </div>
-            </div>
+          <div>
+            <body className={this.props.classes.background}>
+              <div>
+                <Typography variant="h3">GAME RESULTS</Typography>
+              </div>
+              <div>
+                <Typography variant="h5">
+                  SCORE:{" "}
+                  {this.props.gameWrongAnswers.length === undefined
+                    ? 15
+                    : 15 - this.props.gameWrongAnswers.length}
+                  /15{" "}
+                  {parseInt(
+                    ((15 - this.props.gameWrongAnswers.length) / 15) * 100
+                  )}
+                  %
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="h5">
+                  TIME: {this.props.gameTime}
+                </Typography>
+              </div>
+              <div>
+                {this.props.history.location.search && (
+                  <Button>CONTEST LEADERBOARD</Button>
+                )}
+              </div>
+              <div>
+                <ResultsItemCard />
+              </div>
+              <div>
+                <Button onClick={this.playAgain} variant="contained">
+                  CLICK HERE TO PLAY AGAIN
+                </Button>
+              </div>
+              <div>
+                <Button variant="contained">CHECK OUT THE LEADERBOARD</Button>
+              </div>
+            </body>
+          </div>
         );
     }
 }
