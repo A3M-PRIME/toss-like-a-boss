41d26538570adcca3c76e88868a17a733e834995
Michael O'Donnell <gorillaejones@gmail.com>
2019-10-18 11:36:41 -0500

    adds styling to game launch page

--- src/components/CompostBinChoice/CompostBinChoice.jsx
+++ src/components/CompostBinChoice/CompostBinChoice.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { withStyles } from "@material-ui/core/styles";
 
 import Button from "@material-ui/core/Button";
@@ -8,40 +8,62 @@ import Typography from '@material-ui/core/Typography';
 
 
 const styles = {
-    compostDiv: {
-        backgroundColor: "grey",
-        borderRadius: "25px"
-    }
+  compostDiv: {
+    backgroundColor: "grey",
+    borderRadius: "25px",
+    width: 300
+  },
+  compostDivHolderDiv: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  choiceText: {
+    textAlign: 'center'
+  },
+  compostButtons: {
+    display: 'flex',
+    margin: 'auto',
+    width: 100
+  },
+  button: {
+    padding: 10
+  }
 }
 
 class CompostBinChoice extends Component {
-    noCompostClick = () => {
-        this.props.dispatch({
-            type: 'NO_COMPOST_BIN'
-        })
-        this.props.history.push('/game')
-    }
-    render() {
-        return (
-          <div className={this.props.classes.compostDiv}>
-            <Typography variant='h6'>
-              Do you have a compost bin? Please select yes or no:
+  noCompostClick = () => {
+    this.props.dispatch({
+      type: 'NO_COMPOST_BIN'
+    })
+    this.props.history.push('/game')
+  }
+  render() {
+    return (
+      <div className={this.props.classes.compostDivHolderDiv}>
+        <div className={this.props.classes.compostDiv}>
+          <Typography className={this.props.classes.choiceText} variant='h6'>
+            Do you have a compost bin? Please select yes or no:
             </Typography>
-            <Button
-              color='secondary'
-              variant='outlined'
-              onClick={() => this.props.history.push("/game")}>
-              Yes
+            <div className={this.props.classes.compostButtons}>
+          <Button
+          className={this.props.classes.button}
+            color='primary'
+            variant='contained'
+            onClick={() => this.props.history.push("/game")}>
+            Yes
             </Button>
-            <Button
-              color='secondary'
-              variant='outlined'
-              onClick={this.noCompostClick}>
-              No
+          <Button
+              className={this.props.classes.button}
+            color='primary'
+            variant='contained'
+            onClick={this.noCompostClick}>
+            No
             </Button>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect()(withRouter(withStyles(styles)(CompostBinChoice)));
